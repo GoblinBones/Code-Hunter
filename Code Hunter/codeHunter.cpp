546c49031c3f436ec35394e427daf07d9d8b2433
LivingBones <luigarc2@uat.edu>
2019-10-14 21:24:52 -0700

    Add project files.

---
+++ Code Hunter/codeHunter.cpp
@@ -0,0 +1,86 @@
+// CodeHunter.cpp : Defines the entry point for the console application.
+//
+//#include "pch.h" // This line only needed for console C++ project, omit this for Empty C++ Project.
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+int main()
+{
+	string textToAnalyze;
+	// naming integers values
+	int foo = 0;
+	int vowels = 0;
+	int consonants = 0;
+	int digits = 0;
+	int spaces = 0;
+	int lengthOfStringSubmittedForAnalysis = 0;
+	int unknownCharacters = 0;
+	int checkSum = 0;
+	int bar = 0;
+
+	cout << "Welcome to the CIA code Hunter Program!" << endl;
+	cout << "Please type in text to analyze: " << endl;
+	getline(cin, textToAnalyze);
+
+	//function that analyze each 
+	for (int i = 0; i < textToAnalyze.length(); ++i)
+	{
+		//looks to see if there are any vowels
+		if (textToAnalyze[i] == 'a' || textToAnalyze[i] == 'e' || textToAnalyze[i] == 'i' ||
+			textToAnalyze[i] == 'o' || textToAnalyze[i] == 'u' || textToAnalyze[i] == 'A' ||
+			textToAnalyze[i] == 'E' || textToAnalyze[i] == 'I' || textToAnalyze[i] == 'O' ||
+			textToAnalyze[i] == 'U')
+		{
+			//fixed the vowles from negative to positive
+			++vowels;
+		}
+		// looks to see what what is a conssonant
+		else if ((textToAnalyze[i] >= 'a' && textToAnalyze[i] <= 'z') || (textToAnalyze[i] >= 'A' && textToAnalyze[i] <= 'Z'))
+		{
+			//uncommented for consonants to work
+			++consonants;
+		}
+		//looks to see if there are any digits
+		else if (textToAnalyze[i] >= '0' && textToAnalyze[i] <= '9')
+		{
+			++digits;
+		}
+		//looks to see if there are any spaces
+		else if (textToAnalyze[i] == ' ')
+		{
+			++spaces;
+		}
+		else
+		{
+			++unknownCharacters;
+		}
+	}
+
+	lengthOfStringSubmittedForAnalysis = textToAnalyze.length();
+	checkSum = unknownCharacters + vowels + consonants + digits + spaces;
+
+	cout << "Vowels: " << vowels << endl;
+	cout << "Consonants: " << consonants << endl;
+	cout << "Digits: " << digits << endl;
+	cout << "White spaces: " << spaces << endl;
+	cout << "Length of string submitted for analysis: " << lengthOfStringSubmittedForAnalysis << endl;
+	cout << "Number of characters CodeHunter could not identify: " << unknownCharacters << endl;
+	cout << "Checksum: " << checkSum << endl;
+
+	//function to check everything and state if it is working or not 
+	if (checkSum == lengthOfStringSubmittedForAnalysis)
+	{
+		cout << "This program self checking has found this Analysis to be valid." << endl;
+	}
+	else
+	{
+		cout << "WARNING! *** This program self checking has found this Analysis to be invalid! Do not use this data!" << endl;
+	}
+
+	system("pause");
+
+	return 0;
+}
